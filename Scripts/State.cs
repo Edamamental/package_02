1b1694335c0069a48e4c6114e5edd7e52e0884df
Naohiro Kimura <155drops@gmail.com>
2023-03-26 09:51:47 +0900

    ステートマシン用のスクリプトを追加

---
+++ Scripts/State.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StateMachineSystem
+{
+	[System.Serializable]
+	public abstract class State<T>
+	{
+		protected T Owner;
+		private GameStateBase owner;
+
+		public State(T owner)
+		{
+			Owner = owner;
+		}
+
+		protected State(GameStateBase owner)
+		{
+			this.owner = owner;
+		}
+
+		public virtual void StateUpdate(T owner) { }
+		public virtual void StateEnter(T owner) { }
+		public virtual void StateExit(T owner) { }
+		public virtual IEnumerator StateUpdateEn(T owner) { yield break; }
+		public virtual void StateFixeUpdate(T owner) { }
+	}
+
+	public class GameStateBase{}
+
+}
---
+++ Scripts/StateMachine.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StateMachineSystem;
+
+
+namespace StateMachineSystem
+{
+	[System.Serializable]
+	public class StateMachine<T> where T : MonoBehaviour
+	{
+		T Owner;
+		State<T> CurrentStete = null;
+		Coroutine StateCor = null;
+
+		public StateMachine(T owner, State<T> firstState)
+		{
+			Owner = owner;
+			ChangeStete(firstState);
+		}
+
+		public void OnDestroy()
+		{
+			CurrentStete.StateExit(Owner);
+		}
+
+		public void ChangeStete(State<T> nextSteta)
+		{
+			if (Owner == null)
+			{
+				return;
+			}
+				
+			
+			//初回時はステート入っていないのでスキップ
+			if(CurrentStete != null)
+			{
+				CurrentStete.StateExit(Owner);
+			}
+			CurrentStete = nextSteta;
+			CurrentStete.StateEnter(Owner);
+
+			//コルーチンをここで留めるて新しいステートのコルーチンを開始
+			if (StateCor != null && Owner != null)
+			{
+				Owner.StopCoroutine(StateCor);
+			}
+			if(Owner != null && Owner.gameObject.activeSelf)
+			{
+                StateCor = Owner.StartCoroutine(CurrentStete.StateUpdateEn(Owner));
+
+			}
+			
+		}
+
+		public void StateMachineUpdate()
+		{
+			if (Owner == null)
+			{
+				return;
+			}
+			CurrentStete.StateUpdate(Owner);
+		}
+		public void StateMachineFixedUpdate()
+		{
+			if (Owner == null)
+			{
+				return;
+			}
+			CurrentStete.StateFixeUpdate(Owner);
+		}
+
+		public State<T> GetCurrentState()
+		{
+			return CurrentStete;
+		}
+	}
+}
